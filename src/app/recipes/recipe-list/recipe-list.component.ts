4f66a4a0ba7aec1a8ade2de9fb3ad44c74dbf363
Joseph Dinh <JDinh1985@gmail.com>
2019-07-21 23:06:52 -0400

    add angular forms

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,15 +12,13 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) {}
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
   }
 
   onNewRecipe() {
-    this.router.navigate(['new'] {relativeTo: this.route});
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 }
