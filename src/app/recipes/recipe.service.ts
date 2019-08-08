e4e9f814215bd4f19c5e7ba86d3fdcd126c74098
Joe Dinh <joe.dinh.contractor@8451.com>
2019-08-08 10:08:14 -0400

    implement http requests to firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,28 +2,33 @@ import { Recipe } from './recipe.model';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import { Subject } from 'rxjs';
+import { Subject, VirtualTimeScheduler } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-      [new Ingredient('Ground Beef', 1), new Ingredient('Onions', 2)],
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-      [new Ingredient('Cream Cheese', 1), new Ingredient('Flour', 2)],
-    ),
-  ];
+  // private recipes: Recipe[] = [
+    // new Recipe(
+    //   'A Test Recipe',
+    //   'This is simply a test',
+    //   'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    //   [new Ingredient('Ground Beef', 1), new Ingredient('Onions', 2)],
+    // ),
+    // new Recipe(
+    //   'Another Test Recipe',
+    //   'This is simply a test',
+    //   'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    //   [new Ingredient('Cream Cheese', 1), new Ingredient('Flour', 2)],
+    // ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
   getRecipes() {
     return this.recipes.slice();
   }
