af510f3677a4652004717d706b97a06f6c9caf40
Joseph Dinh <JDinh1985@gmail.com>
2019-08-09 19:59:25 -0400

    fully implement auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,46 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
     if (!form.valid) {
       return;
     }
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
     if (this.isLoginMode) {
-      // ...
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signUp(email, password).subscribe( resData => {
+      authObs = this.authService.signUp(email, password);
+    }
+    authObs.subscribe(
+      resData => {
         console.log(resData);
         this.isLoading = false;
-      }, error => {
-        this.error = 'An error occurred.';
+        this.router.navigate(['/recipes']);
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
         this.isLoading = false;
-      });
-      form.reset();
-    }
+      },
+    );
+    form.reset();
   }
 
+  login(email: string, password: string) {}
 }
