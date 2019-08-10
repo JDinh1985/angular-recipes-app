df1c67366cf0d6ceab88d74b516011c72b6a04f3
Joseph Dinh <JDinh1985@gmail.com>
2019-08-10 10:29:46 -0400

    fix bugs

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTr
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
@@ -11,15 +11,15 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
     return this.authService.user.pipe(
+      take(1),
       map(user => {
-        return !!user;
-      }),
-      tap(isAuth => {
-        if (!isAuth) {
-          this.router.navigate(['/auth']);
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
         }
+        return this.router.createUrlTree(['/auth']);
       }),
     );
   }
